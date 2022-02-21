f63dc436ffe47502e7d862026de688d8952cbf91
ssh067 <manner____@naver.com>
2022-02-21 11:17:29 +0900

    첫업로드

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+    $(document).ready(function(){
+        $('.event_w').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+    });
+
+    new WOW().init();
